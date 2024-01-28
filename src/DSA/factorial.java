d75098ef9c9e793f814eba2ca4af4f436bb1dac1
ShashiShukla-001 <shuklashashi2001@gmail.com>
2024-01-28 20:22:32 +0530

    new programs

---
+++ src/DSA/factorial.java
@@ -0,0 +1,24 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static int find_factorial(int n)
+    {
+        int fact = 1;
+        for(int i=1; i<=n; i++)
+        {
+            fact = fact*i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        System.out.println("number? ");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int fact = find_factorial(n);
+        System.out.println("factorial = "+fact);
+    }
+}
